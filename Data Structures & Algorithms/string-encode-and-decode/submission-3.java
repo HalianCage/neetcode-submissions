1faeee662ca72b983c1469b8b09047f4e4287285
HalianCage <151435998+HalianCage@users.noreply.github.com>
2026-06-29 19:03:04 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    List<String> list = new ArrayList<>();
+    public String encode(List<String> strs) {
+        list = strs;
+        return "";
+    }
+
+    public List<String> decode(String str) {
+        return list;
+    }
+}
+
