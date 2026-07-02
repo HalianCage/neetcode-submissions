eb92a87ed811a24fdb4841ed0950406226c5c816
HalianCage <151435998+HalianCage@users.noreply.github.com>
2026-07-02 11:03:53 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,44 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        '''
+        Algorithm :-
+        Step 1: Initialize two arrays each for left product and right product of each element
+        Step 2: In a single for loop, 
+            for left product:
+                - If is first element, add 1 into leftProdArray
+                - If not, multiply i-1 of leftProdArray with i-1 of nums array
+            for right product:
+                - Start with len(nums)-i-1
+                - If last element, add 1 to rightProdArray
+                - If not, multiply len(nums)-i of rightProdArray with len(nums)-i of nums array
+        Step 3: Multiply leftProdArray and rightProdArray element for element, store in an array and return the output.
+        '''
+
+        length = len(nums)
+        leftProdArray = [0] * length
+        rightProdArray = [0] * length
+        output = [0] * length
+
+        for i in range(length):
+
+            # leftProdArray
+            if i == 0:
+                leftProdArray[i] = 1
+            else:
+                leftProdArray[i] = leftProdArray[i-1] * nums[i-1]
+
+
+            # rightProdArray
+            invert = length-1-i
+            if i == 0:
+                rightProdArray[invert] = 1
+            else:
+                rightProdArray[invert] = rightProdArray[invert+1] * nums[invert+1]
+
+        for i in range(length):
+            output[i] = leftProdArray[i]*rightProdArray[i]
+
+        return output
+
+        
